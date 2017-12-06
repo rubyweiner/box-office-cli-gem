6c40d5b0f698fe1b5850c3426d725a1f7b0a429c
Ruby Weiner <rubyweiner796@me.com>
2017-12-06 18:05:29 +0000

    movie information

---
+++ lib/boxoffice/movie.rb
@@ -0,0 +1,25 @@
+class BoxOffice::Movie
+
+  def self.Today
+    movie_1 = Movie.new
+    movie_1_name = "Coco"
+    movie_1_RT = "96%"
+    movie_1_AS = "96%"
+    movie_1_URL = "https://www.rottentomatoes.com/m/coco_2017"
+
+    movie_2 = Movie.new
+    movie_2_name = "Justice League"
+    movie_2_RT = "41%"
+    movie_2_AS = "81%"
+    movie_2_URL = "https://www.rottentomatoes.com/m/justice_league_2017"
+
+    movie_3 = Movie.new
+    movie_3_name = "Wonder"
+    movie_3_RT = "85%"
+    movie_3_AS = "91%"
+    movie_3_URL = "https://www.rottentomatoes.com/m/wonder"
+
+
+  end
+
+end
