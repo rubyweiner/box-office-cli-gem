928f924d491c791d09fca39b895f7dbdda5d25df
Ruby Weiner <rubyweiner796@me.com>
2017-12-06 04:33:53 +0000

    first commit

---
+++ lib/boxoffice/cli/gem.rb
@@ -0,0 +1,9 @@
+require "boxoffice/cli/gem/version"
+
+module Boxoffice
+  module Cli
+    module Gem
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/boxoffice/cli/gem/version.rb
@@ -0,0 +1,7 @@
+module Boxoffice
+  module Cli
+    module Gem
+      VERSION = "0.1.0"
+    end
+  end
+end
