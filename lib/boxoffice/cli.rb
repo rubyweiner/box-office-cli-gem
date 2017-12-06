e11e34748432b1a964e9c216477e71db3ada3829
Ruby Weiner <rubyweiner796@me.com>
2017-12-06 06:37:42 +0000

    minor modifications

--- lib/boxoffice/cli.rb
+++ lib/boxoffice/cli.rb
@@ -1,5 +1,35 @@
-class DailyDeal::CLI
+class BoxOffice::CLI
   def call
-    puts "Today's Best Box Office Movies:"
+    list_movies
+    options
+    goodbye
+  end
+
+  def list_movies
+    puts "Today's Top Box Office Movies:"
+    puts "x"
+    puts "y"
+    puts "z"
+  end
+
+  def options
+    input = nil
+    while input != "exit"
+    puts "Type in the number of the movie you'd like more information on. Type 'list' to see today's top 3 box office movies again. Type 'exit' to quit at any time."
+    input = gets.strip
+    if input == "1"
+      puts "more info on movie 1"
+    elsif input == "2"
+      puts "more info on movie 2"
+    elsif input == "3"
+      puts "more info on movie 3"
+    elsif input == "list"
+      list_movies
+    end
+  end
+  end
+
+  def goodbye
+    puts "Thank you for using the boxoffice-cli-gem!"
   end
 end
