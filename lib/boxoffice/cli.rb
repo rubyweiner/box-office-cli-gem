ae1e94c1b6189ab31cefa9ae29285f1ebb4e33bc
Ruby Weiner <rubyweiner796@me.com>
2017-12-07 02:03:05 +0000

    create framework to scrape from rotten tomatoes website

--- lib/boxoffice/cli.rb
+++ lib/boxoffice/cli.rb
@@ -1,4 +1,4 @@
-class BoxOffice::CLI
+ class BoxOffice::CLI
   def call
     list_movies
     options
@@ -7,28 +7,24 @@ class BoxOffice::CLI
 
   def list_movies
     puts "Today's Top 3 Box Office Movies:"
-    puts "x"
-    puts "y"
-    puts "z"
-
-    @movies =  BoxOffice::Movies.Today
+    @movies =  BoxOffice::Movies.today
+    @movies.each.with_index(1) do |movies, i|
+      puts "#{i}. #{movies.name} - Rotton Tomatoes: #{movies.rt_score} - #{movies.a_score} of Audience liked it."
+    end
   end
 
   def options
     input = nil
     while input != "exit"
-    puts "Type in the number of the movie you'd like more information on. Type 'list' to see today's top 3 box office movies again. Type 'exit' to quit at any time."
-    input = gets.strip
-    if input == "1"
-      puts "more info on movie 1"
-    elsif input == "2"
-      puts "more info on movie 2"
-    elsif input == "3"
-      puts "more info on movie 3"
-    elsif input == "list"
-      list_movies
+      puts "Type in the number of the movie you'd like more information on. Type 'list' to see today's top 3 box office movies again. Type 'exit' to quit at any time."
+      input = gets.strip
+      if input.to_i > 0
+        the_movie = @movies[input.to_i - 1]
+        puts "#{the_movie.name} - Rotton Tomatoes: #{the_movie.rt_score} - #{the_movie.a_score} of Audience liked it."#
+      elsif input == "list"
+        list_movies
+      end
     end
-  end
   end
 
   def goodbye
--- lib/boxoffice/movies.rb
+++ lib/boxoffice/movies.rb
@@ -1,20 +1,20 @@
 class BoxOffice::Movies
   attr_accessor :name, :rt_score, :a_score, :url
 
-  def self.Today
-    movie_1 = Movies.new
+  def self.today
+    movie_1 = self.new
     movie_1.name = "Coco"
     movie_1.rt_score = "96%"
     movie_1.a_score = "96%"
     movie_1.url = "https://www.rottentomatoes.com/m/coco_2017"
 
-    movie_2 = Movies.new
-    movie_2.name = "Justice League"
+    movie_2 = self.new
+    movie_2.name = "Justice"
     movie_2.rt_score = "41%"
     movie_2.a_score = "81%"
     movie_2.url = "https://www.rottentomatoes.com/m/justice_league_2017"
 
-    movie_3 = Movies.new
+    movie_3 = self.new
     movie_2.name = "Wonder"
     movie_3.rt_score = "85%"
     movie_3.a_score = "91%"
