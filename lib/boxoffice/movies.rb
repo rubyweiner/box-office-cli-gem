1189087fdcce49fe1864934fc532e520a0ba0bd4
Ruby Weiner <rubyweiner796@me.com>
2017-12-07 08:02:21 +0000

    scraping from RT website for name, rt_score, url

--- lib/boxoffice/movies.rb
+++ lib/boxoffice/movies.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class BoxOffice::Movies
   attr_accessor :name, :rt_score, :a_score, :url
 
@@ -17,15 +18,16 @@ class BoxOffice::Movies
 
   end
 
-  def self.scrape_RT
-    doc = Nokogiri::HTML(open('https://rottontomatoes.com/browse/in-theaters/'))
-    binding.pry
-    #movie = self.new
-    #movie.name = doc.search("h3.movieTitle").text
-    #movie.rt_score = doc.search("#movie_info span.tMeterScore").text
-    #movie.a_score = doc.search ("#movie_info span.tMeterIcon.tiny").text
-    #movie.url = doc.search("a").first.attr("href")
 
-    movie
+
+  def self.scrape_RT
+    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/"))
+      binding.pry
+        movie.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[i].text
+
+        movie.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[i].text
+
+        movie.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[i].attr("href")
+
   end
 end
