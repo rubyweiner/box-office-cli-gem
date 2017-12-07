eb69fbb3f7b12f6f4da39a25f208a4ecea921089
Ruby Weiner <rubyweiner796@me.com>
2017-12-07 17:08:53 +0000

    movie_2 and movie_3 scraped data

--- lib/boxoffice/movies.rb
+++ lib/boxoffice/movies.rb
@@ -9,17 +9,28 @@ class BoxOffice::Movies
   def self.scrape_mov
     movies = []
     movies << self.scrape_RT
+
   end
 
 
 
   def self.scrape_RT
     doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/"))
-      movie = self.new
-      movie.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[0].text.strip
-      movie.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[0].text.strip
-      movie.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[0].attr("href")
+      movie_1 = self.new
+      movie_1.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[0].text.strip
+      movie_1.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[0].text.strip
+      movie_1.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[0].attr("href")
 
-    movie
+      movie_2 = self.new
+      movie_2.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[1].text.strip
+      movie_2.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[1].text.strip
+      movie_2.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[1].attr("href")
+
+      movie_3 = self.new
+      movie_3.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[2].text.strip
+      movie_3.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[2].text.strip
+      movie_3.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[2].attr("href")
+
+      [movie_1, movie_2, movie_3]
   end
 end
