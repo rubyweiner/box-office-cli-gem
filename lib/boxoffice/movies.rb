be7e6c49b92ea9875260d5c8588f60edce56072d
Ruby Weiner <rubyweiner796@me.com>
2017-12-07 03:39:28 +0000

    bundle install pry

--- lib/boxoffice/movies.rb
+++ lib/boxoffice/movies.rb
@@ -9,15 +9,23 @@ class BoxOffice::Movies
   def self.scrape_mov
     movies = []
 
-    movies << self.scrape_mov
+    movies << self.scrape_RT
     # go to rottentomatoes, find the top three Movies
     # extract the properties of each movies
     # instantiate a movie
 
-     
+
   end
 
   def self.scrape_RT
-    doc = Nokogiri::HTML(open("https://rottontomatoes.com"))
+    doc = Nokogiri::HTML(open('https://rottontomatoes.com/browse/in-theaters/'))
+    binding.pry
+    #movie = self.new
+    #movie.name = doc.search("h3.movieTitle").text
+    #movie.rt_score = doc.search("#movie_info span.tMeterScore").text
+    #movie.a_score = doc.search ("#movie_info span.tMeterIcon.tiny").text
+    #movie.url = doc.search("a").first.attr("href")
 
+    movie
+  end
 end
