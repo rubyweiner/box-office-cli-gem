d9e74edd21627d777e2c1c672044a3a9832b6f74
Ruby Weiner <rubyweiner796@me.com>
2017-12-21 01:22:02 +0000

    scraped critic consensus and a_score in movies, adjusted cli

--- lib/boxoffice/cli.rb
+++ lib/boxoffice/cli.rb
@@ -8,9 +8,9 @@
 
   def list_movies
     puts "Today's Top 3 Box Office Movies:"
-    @movies =  BoxOffice::Movies.today
-    @movies.each.with_index(1) do |movie, i|
-     puts "#{i}. #{movie.name} - Rotton Tomatoes: #{movie.rt_score}"
+    @top_three =  BoxOffice::Movies.today
+    @top_three.each.with_index(1) do |movie, i|
+     puts "#{i}. '#{movie.name}' - Rotton Tomatoes: #{movie.rt_score}"
     end
   end
 
@@ -20,8 +20,11 @@
       puts "Type in the number of the movie you'd like more information on. Type 'list' to see today's top 3 box office movies again. Type 'exit' to quit at any time."
       input = gets.strip
       if input.to_i > 0 && input.to_i < 4
-        movie = @movies[input.to_i - 1]
-        puts "#{movie.name} - Rotton Tomatoes: #{movie.rt_score}"
+        movie = @top_three[input.to_i - 1]
+        puts "'#{movie.name}'"
+        puts "Rotton Tomatoes: #{movie.rt_score}"
+        puts "Audience Score: #{movie.a_score}"
+        puts "#{movie.cc}"
       elsif input == "list"
         list_movies
       end
--- lib/boxoffice/movies.rb
+++ lib/boxoffice/movies.rb
@@ -1,8 +1,9 @@
 class BoxOffice::Movies
-  attr_accessor :name, :rt_score, :url, :movies
+  attr_accessor :name, :rt_score, :url, :top_three, :cc, :a_score
 
   def self.today
-    self.scrape_RT
+    self.mov_info
+    self.more_info
   end
 
 
@@ -14,24 +15,38 @@ class BoxOffice::Movies
 
 #movies.each_slice(3).to_a
 
-  def self.scrape_RT
-    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/"))
+  def self.mov_info
+    doc1 = Nokogiri::HTML(open("https://www.rottentomatoes.com/"))
+
       movie_1 = self.new
-      movie_1.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[0].text.strip
-      movie_1.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[0].text.strip
-      movie_1.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[0].attr("href")
+      movie_1.name = doc1.css("#homepage-top-box-office .movie_list .middle_col")[0].text.strip
+      movie_1.rt_score = doc1.css("#homepage-top-box-office .movie_list .left_col")[0].text.strip
+      movie_1.url = doc1.css("#homepage-top-box-office .movie_list .middle_col a")[0].attr("href")
 
       movie_2 = self.new
-      movie_2.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[1].text.strip
-      movie_2.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[1].text.strip
-      movie_2.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[1].attr("href")
+      movie_2.name = doc1.css("#homepage-top-box-office .movie_list .middle_col")[1].text.strip
+      movie_2.rt_score = doc1.css("#homepage-top-box-office .movie_list .left_col")[1].text.strip
+      movie_2.url = doc1.css("#homepage-top-box-office .movie_list .middle_col a")[1].attr("href")
 
       movie_3 = self.new
-      movie_3.name = doc.css("#homepage-top-box-office .movie_list .middle_col")[2].text.strip
-      movie_3.rt_score = doc.css("#homepage-top-box-office .movie_list .left_col")[2].text.strip
-      movie_3.url = doc.css("#homepage-top-box-office .movie_list .middle_col a")[2].attr("href")
+      movie_3.name = doc1.css("#homepage-top-box-office .movie_list .middle_col")[2].text.strip
+      movie_3.rt_score = doc1.css("#homepage-top-box-office .movie_list .left_col")[2].text.strip
+      movie_3.url = doc1.css("#homepage-top-box-office .movie_list .middle_col a")[2].attr("href")
 
-      movies = [movie_1, movie_2, movie_3]
+      [movie_1, movie_2, movie_3]
   end
+
+
+  def self.more_info
+    top_three = self.mov_info
+     top_three.each.with_index do |movie, i|
+       doc2 = Nokogiri::HTML(open("https://www.rottentomatoes.com#{top_three[i.to_i].url}"))
+       top_three[i.to_i].cc = doc2.css("#scorePanel .col-full-xs").text.strip
+       top_three[i.to_i].a_score = doc2.css("#scorePanel .audience-score .media-body .meter-value").text.strip
+     end
+
+  end
+
+  #{top_three[i.to_i].url}
 
 end
